5e78b4ce1b59ad82831d4573b72008bd9e7c4561
Mateusz Somysz <mateusz.somysz@btcx.com>
2024-04-19 10:54:48 +0200

    Initial commit

---
+++ rsbuild.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
+
+export default defineConfig({
+  server: {
+    port: 2000,
+  },
+  tools: {
+    rspack: (config, { appendPlugins }) => {
+      appendPlugins([
+        new ModuleFederationPlugin({
+          name: 'main-app',
+          remotes: {
+            common:
+                'common@https://somysz.github.io/module-federation-test/mf-manifest.json',
+          },
+          shared: ['react', 'react-dom'],
+
+        }),
+      ]);
+    },
+  },
+  plugins: [pluginReact()],
+});
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// The remote component provided by federation_provider
+import './App.css';
+import ProviderButton from 'common/button';
+
+const App = () => {
+    return (
+        <div>
+            <div>
+                <ProviderButton />
+            </div>
+        </div>
+    );
+};
+
+export default App;
